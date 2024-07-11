fc7d00aa452752e5cf375dd7d839fd0b5916b461
tech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2024-07-11 19:33:26 +0900

    tweak

---
+++ src/datasets.py
@@ -0,0 +1,150 @@
+import re
+from statistics import mode
+
+import torch
+import pandas
+from PIL import Image
+import numpy as np
+
+from .utils import set_seed
+
+set_seed(42)
+
+# テキストの前処理を行う関数
+def process_text(text):
+    # lowercase
+    text = text.lower()
+
+    # 数詞を数字に変換
+    num_word_to_digit = {
+        'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4',
+        'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9',
+        'ten': '10'
+    }
+    for word, digit in num_word_to_digit.items():
+        text = text.replace(word, digit)
+
+    # 小数点のピリオドを削除
+    text = re.sub(r'(?<!\d)\.(?!\d)', '', text)
+
+    # 冠詞の削除
+    text = re.sub(r'\b(a|an|the)\b', '', text)
+
+    # 短縮形のカンマの追加
+    contractions = {
+        "dont": "don't", "isnt": "isn't", "arent": "aren't", "wont": "won't",
+        "cant": "can't", "wouldnt": "wouldn't", "couldnt": "couldn't"
+    }
+    for contraction, correct in contractions.items():
+        text = text.replace(contraction, correct)
+
+    # 句読点をスペースに変換
+    text = re.sub(r"[^\w\s':]", ' ', text)
+
+    # 句読点をスペースに変換
+    text = re.sub(r'\s+,', ',', text)
+
+    # 連続するスペースを1つに変換
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
+
+# 1. データローダーの作成
+class VQADataset(torch.utils.data.Dataset):
+    def __init__(self, df_path, image_dir, transform=None, answer=True):
+        self.transform = transform  # 画像の前処理
+        self.image_dir = image_dir  # 画像ファイルのディレクトリ
+        self.df = pandas.read_json(df_path)  # 画像ファイルのパス，question, answerを持つDataFrame
+        self.answer = answer # answerを使用するかどうかのフラグ
+
+        # question / answerの辞書を作成
+        self.question2idx = {}
+        self.answer2idx = {}
+        self.idx2question = {}
+        self.idx2answer = {}
+
+        # 質問文に含まれる単語を辞書に追加
+        for question in self.df["question"]:
+            question = process_text(question) # 質問文の前処理
+            words = question.split(" ") # 質問文を単語に分割
+            for word in words:
+                if word not in self.question2idx:
+                    self.question2idx[word] = len(self.question2idx) # 辞書に単語を追加
+        self.idx2question = {v: k for k, v in self.question2idx.items()}  # 逆変換用の辞書(question)
+
+        if self.answer:
+            # 回答に含まれる単語を辞書に追加
+            for answers in self.df["answers"]:
+                for answer in answers:
+                    word = answer["answer"]
+                    word = process_text(word) # 回答の前処理
+                    if word not in self.answer2idx:
+                        self.answer2idx[word] = len(self.answer2idx) # 辞書に単語を追加
+            self.idx2answer = {v: k for k, v in self.answer2idx.items()}  # 逆変換用の辞書(answer)
+
+    def update_dict(self, dataset):
+        """
+        検証用データ，テストデータの辞書を訓練データの辞書に更新する．
+
+        Parameters
+        ----------
+        dataset : Dataset
+            訓練データのDataset
+        """
+        self.question2idx = dataset.question2idx
+        self.answer2idx = dataset.answer2idx
+        self.idx2question = dataset.idx2question
+        self.idx2answer = dataset.idx2answer
+
+    def __getitem__(self, idx):
+        """
+        対応するidxのデータ（画像，質問，回答）を取得．
+
+        Parameters
+        ----------
+        idx : int
+            取得するデータのインデックス
+
+        Returns
+        -------
+        image : torch.Tensor  (C, H, W)
+            画像データ
+        question : torch.Tensor  (vocab_size)
+            質問文をone-hot表現に変換したもの
+        answers : torch.Tensor  (n_answer)
+            10人の回答者の回答のid
+        mode_answer_idx : torch.Tensor  (1)
+            10人の回答者の回答の中で最頻値の回答のid
+        """
+        # 画像ファイルを開く
+        image = Image.open(f"{self.image_dir}/{self.df['image'][idx]}")
+        # 画像の前処理を行う
+        image = self.transform(image)
+        # one-hot表現のための配列（未知語用の要素を追加）
+        question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
+        # 質問文を単語に分割
+        question_words = self.df["question"][idx].split(" ")
+        for word in question_words:
+            try:
+                question[self.question2idx[word]] = 1  # one-hot表現に変換
+            except KeyError:
+                question[-1] = 1  # 未知語
+
+        if self.answer:
+            # 回答のリスト
+            answers = [self.answer2idx[process_text(answer["answer"])] for answer in self.df["answers"][idx]]
+            # 最頻値の取得
+            mode_answer_idx = mode(answers)  # 最頻値を取得（正解ラベル）
+
+            # 回答ありの場合の返り値
+            return image, torch.Tensor(question), torch.Tensor(answers), int(mode_answer_idx)
+
+        else:
+            # 回答なしの場合の返り値
+            return image, torch.Tensor(question)
+
+    def __len__(self):
+        """
+        データセットの長さを返す関数
+        """
+        return len(self.df)
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,17 @@ import random
 import numpy as np
 import torch
 
-def set_seed(seed: int = 0) -> None:
+# def set_seed(seed: int = 0) -> None:
+#     random.seed(seed)
+#     np.random.seed(seed)
+#     torch.manual_seed(seed)
+
+# 再現性を担保するためにシードを固定
+def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
